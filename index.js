9a5e35a4dbeaa779f8ecdee1204b17f6ee2878df
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-05-24 19:16:07 +0200

    adding simple qurey

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const {GraphQLObjectType,GraphQLSchema,GraphQLID,GraphQLString}=require('graphql
 const port=8080
 const app=express()
 
+const QueryRoot = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    hello: {
+      type: graphql.GraphQLString,
+      resolve: () => "Hello world!"
+    }
+  })
+})
+    
+const schema = new graphql.GraphQLSchema({ query: QueryRoot })
+
 app.use('/graphql',graphqlHTTP({
-    schema:null,
+    schema,
     graphiql:true
 }))
 
